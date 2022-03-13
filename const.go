aca81b93c3ddcec91eb5148e92ff33fe0abe9fa9
Alexander Smirnov <35576482+olekzonder@users.noreply.github.com>
2022-03-13 20:35:10 +0300

    first version
    very raw, a lot of things to do...

---
+++ const.go
@@ -0,0 +1,5 @@
+package translate
+const ( //TODO: make more languages available and start using the listLanguages method
+	url = "https://translate.api.cloud.yandex.net/translate/v2/translate"
+	detect = "https://translate.api.cloud.yandex.net/translate/v2/detect"
+)
---
+++ translator.go
@@ -0,0 +1,77 @@
+package translate
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Client struct{
+	IAM_TOKEN string
+	folderId string
+}
+
+type Response struct{
+	Code int
+	Message string
+	Lang string
+	Translations []Translations `json:"translations"`
+}
+
+type Translations struct{
+	Text string `json:"text"`
+}
+
+type Body struct{
+	Source string `json:"sourceLanguageCode"`
+	Target string `json:"targetLanguageCode"`
+	Texts []string `json:"texts"`
+	FolderId string `json:"folderId"`
+}
+
+func NewClient(IAM_TOKEN, folderId string) *Client {
+	return &Client{
+		IAM_TOKEN: IAM_TOKEN,
+		folderId: folderId}
+}
+
+func (c *Client) Translate(texts []string, source, target string) ([]string, error){ //TODO: Language Detection, more functionality
+	b := Body{
+		FolderId: c.folderId,
+		Source: source,
+		Target: target,
+		Texts: texts}
+
+	jsonStr, err := json.Marshal(b)
+	if err != nil{
+		panic(err)
+	}
+	request, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "Bearer " + c.IAM_TOKEN)
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil{
+		return nil, err
+	}
+
+	var resp Response
+
+	if err = json.NewDecoder(response.Body).Decode(&resp)
+	err !=nil{
+		return nil,err
+	}
+
+	if response.StatusCode != 200{
+		fmt.Println(resp.Code,resp.Message,response.StatusCode)
+		return nil, err
+	}
+
+	var result []string
+	for _,v := range resp.Translations{
+
+		result = append(result,v.Text)
+	}
+	return result,nil
+}
